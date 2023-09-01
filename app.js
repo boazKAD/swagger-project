fd0dd0bb342c9b8d4a84d43bc00db197d5a29e09
boazKAD <boazkhalifa@gmail.com>
2023-09-01 10:11:52 +0200

    swagger api docs

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 8000;
 const swaggerOptions = {
-    swaggerDefinition: {
-        info:{
-            title: "Customer API",
-            description: "Customer API documentation",
-            contact: {
-                name: "Amazing Developer",
-            },
-        },
-        servers: [
-            {
-                url: `http://localhost:${port}`,
-            },
-        ],
+  swaggerDefinition: {
+    info: {
+      title: "Customer API",
+      description: "Customer API documentation",
+      contact: {
+        name: "Amazing Developer",
+      },
     },
-    apis: ["path-to-your-api-routes-file.js"], // Update this with the correct file path
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+  },
+  // ['.routes/*.js'],
+  apis: ["app.js"], // Update this with the correct file path
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
@@ -27,10 +28,34 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 // Setup Swagger UI route
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.get('/customer', (req, res) => {
-    res.send("customer results");
+app.get("/customer", (req, res) => {
+  res.send("customer results");
 });
 
+// routes
+/**
+ * @swagger
+ * /customer:
+ *  get:
+ *   description: use to request all customers
+ *   responses:
+ *     '200':
+ *       description: a successful response
+ */
+app.put("/customer", (req, res) => {
+    res.send(" customer updated successfully");
+  });
+  
+  // routes
+  /**
+   * @swagger
+   * /customer:
+   *  put:
+   *   description: use to update customers
+   *   responses:
+   *     '200':
+   *       description: a successful response
+   */
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
